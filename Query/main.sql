d360b2c65f1db0647609d4071b7858165acde599
atrkhomeini <102291396+CallMePo@users.noreply.github.com>
2024-01-27 10:36:32 +0700

    Update main.sql

--- Query/main.sql
+++ Query/main.sql
@@ -5,11 +5,10 @@ SELECT
     CONCAT(cus.FirstName, ' ', cus.LastName) AS FullName,
     cus.Country,
     tr.Name as TrackName,
-	alb.Title as AlbumName,
-	art.Name as Artist,
-	gr.Name as Genre,
-    /* Tambah Genre
-    Tambah Artis*/
+    alb.Title as AlbumName,
+    art.Name as Artist,
+    gr.Name as Genre,
+    CONCAT(emp.FirstName, ' ',emp.LastName) as SupportRep,
     ili.Quantity,
     ili.UnitPrice,
     invo.InvoiceDate,
@@ -19,9 +18,10 @@ FROM
     JOIN invoiceline ili ON ili.InvoiceId = invo.InvoiceId
     JOIN customer cus ON invo.CustomerId = cus.CustomerId
     JOIN track tr ON tr.TrackId = ili.TrackId
-	JOIN genre gr ON gr.GenreId=tr.GenreId
-	JOIN album alb ON alb.AlbumId=tr.AlbumId
-	JOIN artist art ON art.ArtistId=alb.ArtistId
+    JOIN genre gr ON gr.GenreId=tr.GenreId
+    JOIN album alb ON alb.AlbumId=tr.AlbumId
+    JOIN artist art ON art.ArtistId=alb.ArtistId
+    JOIN employee emp ON emp.SupportRepId=cus.SupportRepId
 GROUP BY
     invo.InvoiceId,
     ili.InvoiceLineId,
@@ -29,10 +29,11 @@ GROUP BY
     CONCAT(cus.FirstName, ' ', cus.LastName),
     cus.Country,
     tr.Name,
-	alb.Title,
-	art.Name,
-	gr.Name,
+    alb.Title,
+    art.Name,
+    gr.Name,
     ili.Quantity,
     ili.UnitPrice,
+    CONCAT(emp.FirstName, ' ',emp.LastName),
     invo.InvoiceDate,
     invo.Total;
